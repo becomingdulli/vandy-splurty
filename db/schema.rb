583a7dbc03c9cdc1d4d16ef35f223faa28d4592e
Jason <becomingdulli@gmail.com>
2019-03-07 18:58:22 +0000

    added quotes

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,9 @@ ActiveRecord::Schema.define(version: 2019_03_06_192532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "quotes", force: :cascade do |t|
+  create_table "quotes", id: :serial, force: :cascade do |t|
     t.string "saying"
-    t.string "authot"
+    t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
